7a0370f388c88435fdfbeb79a280019133b332cd
satoshix13 <satoshix13@gmail.com>
2020-10-06 15:38:55 +0200

    first push

---
+++ contact-book/src/router.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import ContactList from './views/ContactList.vue'
+import ContactPage from './views/ContactPage.vue'
+
+
+Vue.use(Router)
+
+export default new Router({
+    mode: 'history',
+    routes: [
+      {
+        path: "/",
+        name: "ContactList",
+        component: ContactList,
+      },
+      {
+        path: "/contactpage",
+        name: "ContactPage",
+        component: ContactPage,
+      }
+    ]
+})
---
+++ contact-book/src/views/ContactPage.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <div class="right">
+      <div class="header">
+          <button class="btn" @click="$router.push('/')">CONTACTS</button>
+          <h1>User Info</h1>
+      </div>
+      <div>
+        <input placeholder="fieldname" type="text">
+        <input placeholder="value" type="text">
+        <button> Add </button>
+      </div>
+     <div class="user-info">
+        <h1>hello</h1>
+     </div>
+      
+
+
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'ContactPage'
+}
+</script>
+
+
+<style  scoped>
+input {
+  width: 25%;
+  margin: auto;
+  height: 30px;
+  border-radius: 5px;
+  margin: 0 5%;
+}
+
+ul {
+  list-style: none;
+}
+
+</style>
